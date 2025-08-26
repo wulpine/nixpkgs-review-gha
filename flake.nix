3554d4f59d513ec7ca383302d040483564a1c0f3
Defelo <mail@defelo.de>
2025-08-26 14:33:12 +0200

    refactor: move custom packages into flake

--- flake.nix
+++ flake.nix
@@ -1,24 +1,37 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-review.url = "github:Mic92/nixpkgs-review";
   };
 
   outputs =
-    { self, nixpkgs }:
+    {
+      self,
+      nixpkgs,
+      nixpkgs-review,
+    }:
 
     let
       inherit (nixpkgs) lib;
 
-      eachSystem = f: lib.genAttrs systems (s: f nixpkgs.legacyPackages.${s});
+      eachSystem = f: lib.genAttrs systems (system: f (import nixpkgs { inherit system overlays; }));
       systems = [
         "x86_64-linux"
         "aarch64-linux"
         "x86_64-darwin"
         "aarch64-darwin"
       ];
+
+      overlays = [ self.overlays.nixpkgs-review ];
     in
 
     {
+      overlays.nixpkgs-review = final: prev: {
+        inherit (nixpkgs-review.packages.${final.system}) nixpkgs-review;
+      };
+
+      legacyPackages = eachSystem lib.id;
+
       formatter = eachSystem (
         pkgs:
         pkgs.treefmt.withConfig {
@@ -30,6 +43,7 @@
       );
 
       checks = eachSystem (pkgs: {
+        inherit (pkgs) nixpkgs-review;
         fmt = pkgs.runCommandNoCCLocal "fmt-check" { } ''
           cp -r --no-preserve=mode ${self} repo
           ${lib.getExe self.formatter.${pkgs.system}} -C repo --ci
--- packages.nix
+++
@@ -1,8 +0,0 @@
-let
-  nixpkgs = builtins.getFlake "github:nixos/nixpkgs/nixpkgs-unstable";
-  nixpkgs-review = builtins.getFlake "github:Mic92/nixpkgs-review/eda950b6dc10ae0aeaaaae6b98a55f0040de530e";
-in
-
-import nixpkgs {
-  overlays = [ (final: prev: { inherit (nixpkgs-review.packages.${final.system}) nixpkgs-review; }) ];
-}
